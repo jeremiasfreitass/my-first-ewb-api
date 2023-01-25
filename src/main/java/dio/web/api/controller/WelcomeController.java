8ca6376ec03587a22ee7248e60f47aee0cadd2ab
jeremiasfreitass <jeremiasjmf@hotmail.com>
2023-01-25 19:34:03 -0300

    Correção erro com Swagger

--- src/main/java/dio/web/api/controller/WelcomeController.java
+++ src/main/java/dio/web/api/controller/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    @GetMapping
+    @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to My Spring Boot web API!";
     }
